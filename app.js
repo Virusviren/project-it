1f870c8f3c57f2b79a78034a3f15ecd9bd543db6
Viren Patil <viren@MacBook-Air.local>
2020-03-03 12:10:25 +0100

    Little Update for presentation

--- app.js
+++ app.js
@@ -1,14 +1,28 @@
 //jshint esversion:6
+
 const express = require("express");
+var bodyParser = require('body-parser')
 
 const app = express();
-
+//import{str} from "/index.js";
+//import as str1 from 'index.js';
 app.use(express.urlencoded({extended: true}));
-//app.use(express.static("public"));
-
+app.use(bodyParser.urlencoded({ extended: false }))
 app.use('/', express.static(__dirname + '/'));
+
+
 app.get("/", function(req, res) {
   res.sendFile(__dirname+"/index.html");
   console.log(__dirname);
+  console.log(req.body.str);
+  
 });
-app.listen(3000);
+
+app.post("/",function(req,res){
+  var code = req.body.str;
+  res.sendFile(__dirname+"/index.html");
+})
+app.listen(3000,()=>{
+  console.log("Server Started on port 3000.");
+  
+});
