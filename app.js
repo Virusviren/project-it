09854a9de4d57cba29aebe9ac6a0fac95c2fedc8
Viren Patil <viren@netpanel-212-182-84-147.wsei-ds.lublin.pl>
2020-03-14 22:16:59 +0100

    Error express

--- app.js
+++ app.js
@@ -1,43 +1,90 @@
 //jshint esversion:6
- ///Users/viren/Desktop/Project-it/index.js
+///Users/viren/Desktop/Project-it/index.js
+require("dotenv").config();
 const express = require("express");
-var cors = require('cors')
-var bodyParser = require('body-parser')
+var bodyParser = require("body-parser");
+const axios = require("axios").default;
+let ejs = require('ejs');
 var barcodeId;
+var body1 = "Viren";
 const app = express();
-//import{str} from "/index.js";
-//import as str1 from 'index.js';
-app.use(express.urlencoded({extended: true}));
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use('/', express.static(__dirname + '/'));
+app.set('view engine','ejs');
 
-//const {barcode1} = require(__dirname + "/index.js");
-app.get('/products/:id', cors(), function (req, res, next) {
-  res.json({msg: str})
-  console.log(str);
-  
-})
+app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static("public"));
+
+// app.use("/", express.static(__dirname + "/"));
+
 
 app.get("/", function(req, res) {
-  res.sendFile(__dirname+"/index.html");
-  console.log(__dirname);
-  //console.log(req.body.str);
+  res.render("index");
   
-});
-app.post('/', function(req, res) {
-  //console.log(req.body.text);
-  barcodeId = req.body.text;
-  //console.log('req received');
-  res.redirect('/');
-  console.log(barcodeId);
+  
 });
 
- 
-// app.post("/",function(req,res){
-//   var code = req.body.str;
-//   res.sendFile(__dirname+"/index.html");
-// })
-app.listen(3000,()=>{
-  console.log("Server Started on port 3000.");
+
+app.get("/form1", function(req, res) {
+  res.render("form1",{body1:body1});
+
   
 });
+
+app.post("/", function(req, res) {
+  barcodeId = req.body.text;
+  let result="";
+  
+  console.log(barcodeId);
+  axios({
+    url:
+      "https://vision.googleapis.com/v1/images:annotate?key=" +
+      process.env.API_KEY,
+    method: "post",
+  
+    headers: {
+      "X-Requested-With": "XMLHttpRequest"
+    },
+  
+    data: {
+      requests: [
+        {
+          image: {
+            content: barcodeId
+          },
+          features: [
+            {
+              type: "TEXT_DETECTION"
+            }
+          ]
+        }
+      ]
+    }
+  })
+    .then(response => {
+      productBarcode = response.data.responses[0].fullTextAnnotation.text;
+      //console.log(productBarcode);
+      
+      let reg = /\d+/g;
+       result = productBarcode.match(reg).join("");
+      console.log(result);
+    
+    })
+    .catch(error => {
+      console.log(error.response);
+    });
+   
+   res.redirect("http://localhost:3000/form1");
+   
+});
+
+
+
+
+
+
+
+
+app.listen(3000, () => {
+  console.log("Server Started on port 3000.");
+});
+
